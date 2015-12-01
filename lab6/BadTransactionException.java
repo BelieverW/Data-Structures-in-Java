c059b93bc3c2fdc70369a2670caf7d64bdbcd8ec
BelieverWANG <Daniel_RICE@HLWang.local>
2015-12-01 14:04:03 +0800

    update comments on BadTransactionException.java

--- lab6/BadTransactionException.java
+++ lab6/BadTransactionException.java
@@ -1,14 +1,14 @@
 /* BadTransactionException.java */
 
 /**
- *  Implements an exception that should be thrown for nonexistent accounts.
+ *  Implements an exception that should be thrown for nonsensible amount of money.
  **/
 public class BadTransactionException extends Exception {
 
-  public int money;  // The invalid account number.
+  public int money;  // The invalid amount of money.
 
   /**
-   *  Creates an exception object for nonexistent account "badAcctNumber".
+   *  Creates an exception object for nonsensible amount of money "badmoney".
    **/
   public BadTransactionException(int badmoney) {
     super("Invalid amount of money: " + badmoney);
